061f25f774d7d6130709d6943618062ce0c48b40
Yiğit Güneli <gro.tiiy@yiit.org>
2011-08-09 22:25:01 +0100

    adding comments

--- macir.py
+++ macir.py
@@ -9,34 +9,59 @@ import os
 
 class Point(db.Model):
     """Database object to store points"""
-
     id = iterableStringProperty.iterableStringProperty()
     point = db.GeoPtProperty()        
     
 
 class MainPage(webapp.RequestHandler):
+    """Class for / url"""
+
     def get(self):
+        """Method for get requests"""
         points = Point.all().order("-id")
-        path = os.path.join(os.path.dirname(__file__), 'templates/index.html')
-        self.response.out.write(template.render(path, locals()))
+
+        #Use templates/index.html as the template file
+        template = os.path.join(os.path.dirname(__file__), 'templates/index.html')
+        self.response.out.write(template.render(template, locals()))
+
 
 class Put(webapp.RequestHandler):
+    """Class to put new coordinates on database"""
+
     def getLast(self):
+        """Returns the last point on database"""
         last = Point.all().order("-id").get()
         return last
 
     def putPoint(self,p):
+        """
+        Puts a db.GeoPt object to database with a generated id
+        Returns id
+        """
+        #Initialize a new Point
         point = Point()
         point.point = p
-        point.id = self.getLast().id.next()
+        point.id = self.getLast().id.next() #next() method comes from iterableString class
+        
+        #Save it on db
         point.put()
+
         return point.id
 
     def get(self):
+        """Method to handle get requests"""
+
+        #Get latitude & longitude from request
         la = self.request.get("la")
         lo = self.request.get("lo")
+
+        #Initialize a new GeoPt object with that values
         point = db.GeoPt(la,lo)
+
+        #Write it on db, get the id
         id = self.putPoint(point)
+
+        #Simply write it to page
         self.response.out.write(id)
 
 
